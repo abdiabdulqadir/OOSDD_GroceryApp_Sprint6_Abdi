f8502e79a411cccd447a82b3b419b84c373efcbc
abdiabdulqadir <abdi.abdulqadir@windesheim.nl>
2025-10-22 14:43:27 +0200

    de views en services aangepast voor uc19

--- Grocery.App/ViewModels/ProductViewModel.cs
+++ Grocery.App/ViewModels/ProductViewModel.cs
@@ -1,19 +1,53 @@
-﻿using Grocery.Core.Interfaces.Services;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Grocery.Core.Interfaces.Services;
 using Grocery.Core.Models;
 using System.Collections.ObjectModel;
 
 namespace Grocery.App.ViewModels
 {
-    public class ProductViewModel : BaseViewModel
+    public partial class ProductViewModel : BaseViewModel
     {
         private readonly IProductService _productService;
+        private readonly GlobalViewModel _globalViewModel;
+
         public ObservableCollection<Product> Products { get; set; }
 
-        public ProductViewModel(IProductService productService)
+        [ObservableProperty]
+        private bool isAdmin = false;
+
+        public ProductViewModel(IProductService productService, GlobalViewModel globalViewModel)
         {
             _productService = productService;
+            _globalViewModel = globalViewModel;
             Products = [];
-            foreach (Product p in _productService.GetAll()) Products.Add(p);
+            IsAdmin = _globalViewModel.Client?.Role == Role.Admin;
+            Load();
+        }
+
+        private void Load()
+        {
+            Products.Clear();
+            foreach (Product p in _productService.GetAll())
+            {
+                Products.Add(p);
+            }
+        }
+
+        [RelayCommand]
+        public async Task AddNewProduct()
+        {
+            await Shell.Current.GoToAsync(nameof(Views.NewProductView), true);
+        }
+
+        public override void OnAppearing()
+        {
+            Load();
+        }
+
+        public override void OnDisappearing()
+        {
+            Products.Clear();
         }
     }
 }
